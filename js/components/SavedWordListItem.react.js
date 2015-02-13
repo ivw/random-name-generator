d1a56540f068cb17f943b62341b66b12a19e13b2
Ivo <ivo@reyez.nl>
2015-02-13 01:15:43 +0100

    Google link in saved words

--- js/components/SavedWordListItem.react.js
+++ js/components/SavedWordListItem.react.js
@@ -8,11 +8,15 @@ var SavedWordListItem = React.createClass({
 		SavedWordStore.remove(this.props.word);
 	},
 	render: function () {
+		var googleLink = "https://www.google.nl/search?q=" + encodeURIComponent(this.props.word);
 		return (
 			<div className="word-list-item">
 				{this.props.word}
 				<a href="javascript:;" onClick={this.handleClick}>
 					<i className="fa fa-times"></i>
+				</a>
+				<a href={googleLink} target="_blank">
+					<i className="fa fa-google"></i>
 				</a>
 			</div>
 		);
